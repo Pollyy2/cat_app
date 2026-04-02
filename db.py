3722a1a588e0eb1129fa51ab56cd751fe439e225
Pollyy2 <polina.1998@abv.bg>
2026-04-03 02:05:39 +0300

    fix db connection for render

--- db.py
+++ db.py
@@ -1,50 +1,43 @@
 import os
-
 import MySQLdb
 
-db = MySQLdb.connect(
-    host="cat-app1-abv-fe39.i.aivencloud.com",
-    user="avnadmin",
-    password = os.getenv("DB_PASSWORD"),
-    database="defaultdb",
-    port=19532,
-    ssl={"ssl": {"ca": ""}}  
-)
-
-cursor = db.cursor()
-
+def get_db():
+    return MySQLdb.connect(
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DB"),
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        ssl={"ssl": {}}
+    )
 
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS cats (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    cat_name VARCHAR(100),
-    cat_age INT,
-    cat_breed VARCHAR(100),
-    contact VARCHAR(100),
-    image VARCHAR(255),
-    user_id INT
-)
-""")
+def init_db():
+    db = get_db()
+    cursor = db.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS accounts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50),
-    password VARCHAR(255),
-    email VARCHAR(100)
-)
-""")
-cursor.execute("SHOW TABLES")
-for table in cursor.fetchall():
-    print(table)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS cats (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        cat_name VARCHAR(100),
+        cat_age INT,
+        cat_breed VARCHAR(100),
+        contact VARCHAR(100),
+        image VARCHAR(255),
+        user_id INT
+    )
+    """)
 
-cursor.execute("SELECT * FROM cats")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS accounts (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(50),
+        password VARCHAR(255),
+        email VARCHAR(100)
+    )
+    """)
 
-db.commit()
-db.close()
+    db.commit()
+    db.close()
 
 
